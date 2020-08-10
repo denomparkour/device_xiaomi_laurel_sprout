e735602442616c1d0d9f8ae8bd99af70af6bbd56
denomparkour <denomparkour@gmail.com>
2020-08-10 05:58:14 +0000

    Add xtended props

---
+++ xtended_laurel_sprout.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from laurel_sprout device
+$(call inherit-product, device/xiaomi/laurel_sprout/device.mk)
+
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := xtended_laurel_sprout
+PRODUCT_DEVICE := laurel_sprout
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi A3
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Xtended Official Props
+XTENDED_MAINTAINER := denomparkour
+XTENDED_BUILD_TYPE := OFFICIAL
+
+# Xtended Props
+EXTRA_FOD_ANIMATIONS := true
+
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="laurel_sprout-user 10 QKQ1.190910.002 V11.0.15.0.QFQMIXM release-keys" \
+    PRODUCT_NAME="laurel_sprout"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
